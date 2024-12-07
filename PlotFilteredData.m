61941dbadafacb3350d93a205ef4f4bcfaca8707
Kristin Dickerson <kdickerson97@gmail.com>
2024-12-06 16:16:30 -0800

    updates following the Sept cruise
    fixed bugs found by AF on Sept cruise; updated GUI; added more versatility for filtering data; new universal input data format as a .mat file, must use a external script to get .raw text files to the .mat input format now

--- PlotFilteredData.m
+++ PlotFilteredData.m
@@ -1,21 +1,27 @@
 function [Checkboxes, TempPlots, BadSensors] = PlotData(figure_Main, H, DATA, panel_Thermistors)
 
 %% Initialize Loading Dialog
-loading = uiprogressdlg(figure_Main, 'Message', 'Plotting raw data...');
+loading = uiprogressdlg(figure_Main, 'Message', 'Plotting filtered data...');
 loading.Value = 0.6;
 
 %% Extract Relevant Data
 NoTherm = H.Fileinfo.No_Thermistors.Value;
 
-Depth = DATA.Depth;
-G = DATA.G;
-Tilt = DATA.Tilt;
+T_dec = DATA.Tdec;
+T_decRaw = DATA.TdecRaw;
+Depth_dec = DATA.Depth_dec;
+G_dec = DATA.G_dec;
+Tilt_dec = DATA.Tilt_dec;
+Time_dec = DATA.Time_dec;
+T_cln = DATA.Tcln;
 T = DATA.Traw;
 Time = DATA.Time;
-TempPlots=[];
 
 %% Round Data
+T_cln = round(T_cln, 3);
+T_dec = round(T_dec, 3);
 T = round(T, 3);
+T_decRaw = round(T_decRaw, 3);
 
 %% Enable Buttons and Set Text
 H.Selections.Select.Enable = 'on';
@@ -29,41 +35,51 @@ colormap(H.Axes.Raw, CMap);
 CMap = [CMap; [0 0 0]];
 
 %% Create Handles for Plotting (One-time Plot Creation)
+h_axDec = gobjects(1, NoTherm);
+h_axCln = gobjects(1, NoTherm);
 h_axRaw = gobjects(1, NoTherm);
+h_axRawDec = gobjects(1, NoTherm);
 
 for i = 1:NoTherm
-    h_axRaw(i) = plot(H.Axes.Raw, Time, T(i,:), '*', 'markersize', 4, 'color', CMap(i,:));
+    h_axDec(i) = plot(H.Axes.Raw, Time_dec, T_dec(i,:), 'o', 'markersize', 3, 'color', CMap(i,:), 'markerfacecolor', CMap(i,:), 'Visible','on');
     hold(H.Axes.Raw, 'on');
+    h_axCln(i) = plot(H.Axes.Raw, Time, T_cln(i,:), 'o', 'markersize', 3, 'color', CMap(i,:), 'markerfacecolor', CMap(i,:), 'Visible','off');
+    h_axRaw(i) = plot(H.Axes.Raw, Time, T(i,:), '*', 'markersize', 4, 'color', CMap(i,:), 'Visible','off');
+    h_axRawDec(i) = plot(H.Axes.Raw, Time_dec, T_decRaw(i,:), '-o', 'markersize', 3, 'color', CMap(i,:), 'markerfacecolor', CMap(i,:), 'Visible','off');
 end
 
 H.Plot_Controls.RawPlot.UserData = h_axRaw;
+H.Plot_Controls.RawDecimatedPlot.UserData = h_axRawDec;
+H.Plot_Controls.CleanPlot.UserData = h_axCln;
+H.Plot_Controls.DecimatedPlot.UserData = h_axDec;
 
 %% Use `set` for Updating Data Dynamically
 % Dynamically set the data instead of creating new plots during zoom
 for i = 1:NoTherm
     % Replace plot data with set, this avoids re-plotting and improves performance
+    set(h_axDec(i), 'YData', T_dec(i,:));
+    set(h_axCln(i), 'YData', T_cln(i,:));
     set(h_axRaw(i), 'YData', T(i,:));
+    set(h_axRawDec(i), 'YData', T_decRaw(i,:));
 end
 
 %% Zoom Optimization Using `axis` and `set` for Data Limits
-%zoomListener = addlistener(H.Axes.Raw, 'XLim', 'PostSet', @(src, evt) updatePlotLimits(src,h_axRaw));
+%zoomListener = addlistener(H.Axes.Raw, 'XLim', 'PostSet', @(src, evt) updatePlotLimits(src, h_axDec, h_axCln, h_axRaw, h_axRawDec));
 
 %% Callback Function to Update Plot Limits Dynamically
-function updatePlotLimits(src, ~, ~, h_axRaw, ~)
+function updatePlotLimits(src, h_axDec, h_axCln, h_axRaw, h_axRawDec)
     % Get new axis limits after zoom
     xLimits = src.XLim;
 
     % Efficiently update data visibility without redrawing entire plot
-    for i = 1:length(h_axRaw)
+    for i = 1:length(h_axDec)
         % Update the data visibility based on the new zoom limits
+        set(h_axDec(i), 'XData', Time_dec(Time_dec >= xLimits(1) & Time_dec <= xLimits(2)));
+        set(h_axCln(i), 'XData', Time(Time >= xLimits(1) & Time <= xLimits(2)));
         set(h_axRaw(i), 'XData', Time(Time >= xLimits(1) & Time <= xLimits(2)));
+        set(h_axRawDec(i), 'XData', Time_dec(Time_dec >= xLimits(1) & Time_dec <= xLimits(2)));
     end
 end
-
-%% Plot Depth and Acceleration Data (No Need for Frequent Updates)
-h_axDep = plot(H.Axes.Depth, Time, -Depth, 'b');
-h_axAcc = plot(H.Axes.Accel, Time, G, 'k', 'Visible', 'off');
-h_axTilt = plot(H.Axes.Tilt, Time, Tilt, 'r');
 
 %% Link Axes and Set Axis Properties
 linkaxes([H.Axes.Raw, H.Axes.Depth, H.Axes.Accel, H.Axes.Tilt], 'x');
@@ -79,7 +95,7 @@ loading.Message = 'Finishing...';
 loading.Value = 0.9;
 
 %% Create Sensor Checkbox Panels
-Checkboxes = createSensorCheckboxes(NoTherm, panel_Thermistors, CMap, [], [], h_axRaw, [], H);
+Checkboxes = createSensorCheckboxes(NoTherm, panel_Thermistors, CMap, h_axDec, h_axCln, h_axRaw, h_axRawDec, H);
 
     function Checkboxes = createSensorCheckboxes(NoTherm, panel_Thermistors, CMap, h_axDec, h_axCln, h_axRaw, h_axRawDec, H)
             %% Plot sensor checkboxes
@@ -123,18 +139,20 @@ Checkboxes = createSensorCheckboxes(NoTherm, panel_Thermistors, CMap, [], [], h_
                   for i=NumberOfSensors:-1:1
                           Checkboxes{i} = uicheckbox(SensorGrids{i}, 'Text', ['T' num2str(i)],'FontSize', 14,'FontWeight', 'bold','FontColor',[1 1 1],...
                               'Value',true, 'Tooltip', ['Check to display data from sensor T' num2str(i) ' on plots'], ...
-                              'tag',['c_b_' num2str(i)], 'ValueChangedFcn', {@cbSensValueChange, [], [], h_axRaw(i), [] ...
+                              'tag',['c_b_' num2str(i)], 'ValueChangedFcn', {@cbSensValueChange, h_axDec(i), h_axCln(i), h_axRaw(i), h_axRawDec(i) ...
                               H.PlotCheckboxes.Filtered_check.Value, H.PlotCheckboxes.Filtereddec_check.Value, H.PlotCheckboxes.Raw_check.Value, ...
                               H.PlotCheckboxes.Rawdec_check.Value});
                   end
     end
 
+%% Save Plot Handles
+TempPlots = struct('FilteredDecimated', h_axDec, 'Filtered', h_axCln, 'RawDecimated', h_axRawDec, 'Raw', h_axRaw);
 
 %% Hide Data for Bad (NaN) Sensors
-BadSensors = hideBadData(T, figure_Main, Checkboxes, [], [], h_axRaw, []);
+BadSensors = hideBadData(T, figure_Main, Checkboxes, h_axDec, h_axCln, h_axRaw, h_axRawDec);
 
 %% Helper Function: Hide Data for Bad (NaN) Sensors
-function BadSensors = hideBadData(T, figure_Main, Checkboxes, ~, ~, h_axRaw, ~)
+function BadSensors = hideBadData(T, figure_Main, Checkboxes, h_axDec, h_axCln, h_axRaw, h_axRawDec)
     T(isnan(T)) = -999;
     BadSensors = find(all(T == -999, 2));
     if ~isempty(BadSensors)
@@ -143,7 +161,10 @@ function BadSensors = hideBadData(T, figure_Main, Checkboxes, ~, ~, h_axRaw, ~)
         if strcmp(hidedata, 'Hide NaN (T = -999) data')
             for i = 1:length(BadSensors)
                 Checkboxes{BadSensors(i)}.Visible = 'off';
+                set(h_axDec(BadSensors(i)), 'Visible', 'off');
+                set(h_axCln(BadSensors(i)), 'Visible', 'off');
                 set(h_axRaw(BadSensors(i)), 'Visible', 'off');
+                set(h_axRawDec(BadSensors(i)), 'Visible', 'off');
             end
         end
     end
